460d00b0caf65a4483d3e029495cc9340bb3e785
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2019-02-13 22:25:48 +0100

    Use trick to force knobs to resend signals when machine is newly connected.
    The value is forced to change one step down and then up.
    
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

--- qtmips_gui/peripheralsview.cpp
+++ qtmips_gui/peripheralsview.cpp
@@ -21,7 +21,21 @@ PeripheralsView::~PeripheralsView()
 }
 
 void PeripheralsView::setup(const machine::PeripSpiLed *perip_spi_led) {
+    int val;
+
     connect(ui->spinRed, SIGNAL(valueChanged(int)), perip_spi_led, SLOT(red_knob_update(int)));
     connect(ui->spinGreen, SIGNAL(valueChanged(int)), perip_spi_led, SLOT(green_knob_update(int)));
     connect(ui->spinBlue, SIGNAL(valueChanged(int)), perip_spi_led, SLOT(blue_knob_update(int)));
+
+    val = ui->spinRed->value();
+    ui->spinRed->setValue(val - 1);
+    ui->spinRed->setValue(val);
+
+    val = ui->spinGreen->value();
+    ui->spinGreen->setValue(val - 1);
+    ui->spinGreen->setValue(val);
+
+    val = ui->spinBlue->value();
+    ui->spinBlue->setValue(val - 1);
+    ui->spinBlue->setValue(val);
 }
