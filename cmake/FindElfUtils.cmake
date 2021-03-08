4d934f7669a8468dc03cd974323c7003606fbe5f
Jakub Dupak <dev@jakubdupak.com>
2021-03-09 00:51:49 +0100

    Project: introduce basic cmake

---
+++ cmake/FindElfUtils.cmake
@@ -0,0 +1,79 @@
+# Source (GPLv2): https://github.com/SimonKagstrom/kcov/blob/master/cmake/FindElfutils.cmake
+
+# - Try to find libdwarf
+# Once done this will define
+#
+#  LIBDWARF_FOUND - system has libdwarf
+#  LIBDWARF_INCLUDE_DIRS - the libdwarf include directory
+#  LIBDWARF_LIBRARIES - Link these to use libdwarf
+#  LIBDWARF_DEFINITIONS - Compiler switches required for using libdwarf
+#
+
+# Locate libelf library at first
+if (NOT LIBELF_FOUND)
+    find_package(LibElf)
+endif (NOT LIBELF_FOUND)
+
+if (LIBDWARF_LIBRARIES AND LIBDWARF_INCLUDE_DIRS)
+    set(LibDwarf_FIND_QUIETLY ON)
+endif (LIBDWARF_LIBRARIES AND LIBDWARF_INCLUDE_DIRS)
+
+find_package(PkgConfig QUIET)
+
+if (PKG_CONFIG_FOUND)
+    set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH ON)
+    pkg_check_modules(PC_LIBDW QUIET libdw)
+endif ()
+
+find_path(DWARF_INCLUDE_DIR
+        NAMES
+        dwarf.h
+        HINTS
+        ${PC_LIBDW_INCLUDE_DIRS}
+        PATHS
+        /usr/include
+        /usr/local/include
+        /opt/local/include
+        /sw/include
+        ENV CPATH) # PATH and INCLUDE will also work
+find_path(LIBDW_INCLUDE_DIR
+        NAMES
+        elfutils/libdw.h
+        HINTS
+        ${PC_LIBDW_INCLUDE_DIRS}
+        PATHS
+        /usr/include
+        /usr/local/include
+        /opt/local/include
+        /sw/include
+        ENV CPATH)
+if (DWARF_INCLUDE_DIR AND LIBDW_INCLUDE_DIR)
+    set(LIBDWARF_INCLUDE_DIRS ${DWARF_INCLUDE_DIR} ${LIBDW_INCLUDE_DIR})
+endif (DWARF_INCLUDE_DIR AND LIBDW_INCLUDE_DIR)
+
+find_library(LIBDW_LIBRARY
+        NAMES
+        dw
+        HINTS
+        ${PC_LIBDW_LIBRARY_DIRS}
+        PATHS
+        /usr/lib
+        /usr/local/lib
+        /opt/local/lib
+        /sw/lib
+        ENV LIBRARY_PATH   # PATH and LIB will also work
+        ENV LD_LIBRARY_PATH)
+
+include(FindPackageHandleStandardArgs)
+
+
+# handle the QUIETLY and REQUIRED arguments and set LIBDWARF_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(ElfUtils DEFAULT_MSG
+        LIBDW_LIBRARY
+        LIBDW_INCLUDE_DIR)
+
+mark_as_advanced(LIBDW_INCLUDE_DIR LIBDW_LIBRARY)
+
+set(LIBDW_LIBRARIES ${LIBDW_LIBRARY})
+set(LIBDW_INCLUDE_DIRS ${LIBDW_INCLUDE_DIR})
---
+++ cmake/FindLibElf.cmake
@@ -0,0 +1,70 @@
+# Source (GPLv2): https://github.com/SimonKagstrom/kcov/blob/master/cmake/FindLibElf.cmake
+
+# - Try to find libelf
+# Once done this will define
+#
+#  LIBELF_FOUND - system has libelf
+#  LIBELF_INCLUDE_DIRS - the libelf include directory
+#  LIBELF_LIBRARIES - Link these to use libelf
+#  LIBELF_DEFINITIONS - Compiler switches required for using libelf
+#
+#  Copyright (c) 2008 Bernhard Walle <bernhard.walle@gmx.de>
+#
+#  Redistribution and use is allowed according to the terms of the New
+#  BSD license.
+#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+if (LIBELF_LIBRARIES AND LIBELF_INCLUDE_DIRS)
+    set(LibElf_FIND_QUIETLY ON)
+endif (LIBELF_LIBRARIES AND LIBELF_INCLUDE_DIRS)
+
+find_package(PkgConfig QUIET)
+
+if (PKG_CONFIG_FOUND)
+    set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH ON)
+    pkg_check_modules(PC_LIBELF QUIET libelf)
+endif ()
+
+find_path(LIBELF_INCLUDE_DIR
+        NAMES
+        libelf.h
+        HINTS
+        ${PC_LIBELF_INCLUDE_DIRS}
+        PATHS
+        /usr/include
+        /usr/include/libelf
+        /usr/local/include
+        /usr/local/include/libelf
+        /opt/local/include
+        /opt/local/include/libelf
+        /sw/include
+        /sw/include/libelf
+        ENV CPATH)
+
+find_library(LIBELF_LIBRARY
+        NAMES
+        elf
+        HINTS
+        ${PC_LIBELF_LIBRARY_DIRS}
+        PATHS
+        /usr/lib
+        /usr/local/lib
+        /opt/local/lib
+        /sw/lib
+        ENV LIBRARY_PATH
+        ENV LD_LIBRARY_PATH)
+
+include(FindPackageHandleStandardArgs)
+
+
+# handle the QUIETLY and REQUIRED arguments and set LIBELF_FOUND to TRUE if all listed variables are TRUE
+find_package_handle_standard_args(LibElf DEFAULT_MSG
+        LIBELF_LIBRARY
+        LIBELF_INCLUDE_DIR)
+
+
+mark_as_advanced(LIBELF_INCLUDE_DIR LIBELF_LIBRARY)
+
+set(LIBELF_LIBRARIES ${LIBELF_LIBRARY})
+set(LIBELF_INCLUDE_DIRS ${LIBELF_INCLUDE_DIR})
