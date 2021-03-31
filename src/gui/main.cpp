2b325794359388649da0ac2535fb210db9d6e2d8
Jakub Dupak <dev@jakubdupak.com>
2021-03-31 17:21:57 +0200

    Project: set properties (like app version) from cmake

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -4,8 +4,11 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    QApplication::setApplicationName("gui");
-    QApplication::setApplicationVersion("0.7.5");
+    // There constants are set in CMake.
+    QApplication::setApplicationName(APP_NAME);
+    QApplication::setOrganizationName(APP_ORGANIZATION);
+    QApplication::setOrganizationDomain(APP_ORGANIZATION_DOMAIN);
+    QApplication::setApplicationVersion(APP_VERSION);
 
     MainWindow w;
     w.start();
