36d1447978e0e806607e35e8d4193efb93a2bc1a
Jakub Dupak <dev@jakubdupak.com>
2021-05-17 15:34:20 +0200

    Packaging: update release script

--- cmake/AddFileHashes.cmake
+++ cmake/AddFileHashes.cmake
@@ -4,7 +4,7 @@
 # Example:
 # cmake -DTEMPLATE=PKGBUILD.in -DOUTPUT=PKGBUILD -DFILE=qtrvsim_0.8.0.tar.xz -P AddFileHashes.cmake
 #
-# See extras/packages/arch/PKGBUILD.in for template examples.
+# See extras/packaging/arch/PKGBUILD.in for template examples.
 # Note that most files are configured (injected with information) twice:
 # First, during configure phase, package information is added and FILE_*
 # placeholders are left intact. Second, after source packing, FILE_*
