c55ed6f8ba1c31107eb4f2a6ca650c3177784d6c
Jakub Dupak <dev@jakubdupak.com>
2021-05-27 00:04:50 +0200

    Project: build debug as default and expose build type as simple variable

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -1,16 +1,19 @@
 # Source (BSD-3) https://github.com/openchemistry/tomviz/blob/master/cmake/BuildType.cmake
 
 # Set a default build type if none was specified
-set(default_build_type "Release")
-if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
-  set(default_build_type "Debug")
-endif()
+set(default_build_type "Debug")
 
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-  message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
-  set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
-      STRING "Choose the type of build." FORCE)
-  # Set the possible values of build type for cmake-gui
-  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release"
-    "MinSizeRel" "RelWithDebInfo")
+	message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+	set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
+	    STRING "Choose the type of build." FORCE)
+	# Set the possible values of build type for cmake-gui
+	set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release"
+	             "MinSizeRel" "RelWithDebInfo")
+endif()
+
+# Set variable to detect given build type
+if(CMAKE_BUILD_TYPE)
+    string(TOUPPER "${CMAKE_BUILD_TYPE}" _upper_build_type)
+    set(BUILD_${_upper_build_type} 1)
 endif()
