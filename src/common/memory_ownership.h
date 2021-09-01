356d8d3c1476941b490409dad9ab65974501442d
Jakub Dupak <dev@jakubdupak.com>
2021-09-01 17:58:17 +0200

    Util: memory_ownership lib

---
+++ src/common/memory_ownership.h
@@ -0,0 +1,26 @@
+/**
+ * Manual memory ownership toolkit.
+ */
+#ifndef QTRVSIM_MEMORY_OWNERSHIP_H
+#define QTRVSIM_MEMORY_OWNERSHIP_H
+
+#include <QScopedPointer>
+
+/**
+ * Tag for pointer owned by someone else. It is recommended to mention owner
+ * in comment to make lifetimes manually verifiable.
+ */
+#define BORROWED
+
+/**
+ * Pointer with static lifetime.
+ */
+#define STATIC
+
+/**
+ * Owned pointer deallocated by automatic destructor.
+ */
+template<typename T>
+using Box = QScopedPointer<T>;
+
+#endif // QTRVSIM_MEMORY_OWNERSHIP_H
