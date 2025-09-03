72dfd34b0529445fcec87e9cb2a7118c9e006280
Adam Němec <nemecad4@fel.cvut.cz>
2025-09-03 22:02:09 +0200

    GUI: add "As CPU (VMA)" memory access view
    Introduce an "As CPU (VMA)" access option in the cached
    access selector to render memory contents as observed
    by the CPU through the frontend interface.

--- src/machine/machine.h
+++ src/machine/machine.h
@@ -92,6 +92,14 @@ public:
     bool get_step_over_exception(enum ExceptionCause excause) const;
     enum ExceptionCause get_exception_cause() const;
 
+    Address virtual_to_physical(Address v) {
+        if (tlb_data) {
+            return tlb_data->translate_virtual_to_physical(v);
+        } else {
+            return v;
+        }
+    }
+
 public slots:
     void play();
     void pause();
